7a57b106285ba15c7f178e3ae6df9b79085b26df
romy <romy@snowyla.com>
2023-12-16 20:49:15 -0800

    adding sprite animations based on movement

--- player.gd
+++ player.gd
@@ -1,6 +1,8 @@
 extends Area2D
 @export var speed = 350
 var velocity = Vector2.ZERO
+
+# will help limit the character's movement
 var screensize = Vector2(480, 720)
 
 # Called when the node enters the scene tree for the first time.
@@ -10,4 +12,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	position += velocity * speed * delta
+	
+	# limit the player's position
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+	
+	if velocity.length() > 0:
+		$AnimatedSprite2D.animation = "run"
+	else:
+		$AnimatedSprite2D.animation = "idle"
+	if velocity.x != 0:
+		$AnimatedSprite2D.flip_h = velocity.x < 0
